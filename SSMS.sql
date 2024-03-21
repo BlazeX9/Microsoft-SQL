fc9009511403a4b8592cd10b54b68c48239dd335
Abhik Chatterjee <chatterjeeabhik94@gmail.com>
2024-03-21 13:40:00 +0530

    Add files via upload

---
+++ SSMS.sql
@@ -0,0 +1,19 @@
+Microsoft SQL Server is a platfrom dependent backend application which only works on windows
+Oravel SQL Server is a platfrom independent backend application
+
+--+-Server Types--+-
+SSMS -> Database Engine
+SSAS -> Analysis Services -> Datawarhousing, to show table format data into 3D graphical format
+SSRS -> Reporting Services -> To export the data as a document (Excel, PDF) 
+SSIS -> Intrigration Services -> To convert one type database into another type database (SQL server to Oracel)
+
+
+
+
+
+
+
+
+
+
+
